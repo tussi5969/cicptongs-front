1c7c8b3ac44028711d6412c29d38d35ea6c626fd
tussi5969 <sakura.0304.0226@gmail.com>
2021-07-31 22:18:09 +0900

    #1 [Add] google map APIキーの取得

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
+    libraries: 'places',
+  },
+})
 
 new Vue({
   render: h => h(App),
