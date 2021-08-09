1ea36a13d75b23b254964d065de4c923fcef18d8
tussi5969 <sakura.0304.0226@gmail.com>
2021-08-09 23:42:33 +0900

    #1 [Add] マーカーを地図上にのプロット

--- src/components/modules/Map.vue
+++ src/components/modules/Map.vue
@@ -1,29 +1,42 @@
 <template>
-  <gmap-map
-      :center="center"
-      :zoom="16"
-      style="width: 100%; height: 640px"
-  >
-    <gmap-marker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      :draggable="true"
-      @click="center=m.position"
-    />
-  </gmap-map>
+  <div>
+    <gmap-map
+        :center="center"
+        :zoom="18"
+        style="width: 100%; height: 640px"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(g, index) in garbages"
+        :position="{lat: g.latitude, lng: g.longitude}"
+        :clickable="true"
+        :draggable="false"
+      />
+    </gmap-map>
+    <!-- <div v-for="(garbage, index) in garbages" :key="index"> -->
+      <!-- <div>{{garbages.index}}</div> -->
+      <!-- <div>{{garbage.latitude}}</div> -->
+    <!-- </div> -->
+  </div>
 </template>
 <script>
+import {db} from '../../plugins/firebase'
 export default {
   data() {
     return {
-      center: { lat: 34.684346, lng: 135.836375 },
-      markers: [
-        {position: {lat: 34.684, lng: 135.836}},
-        {position: {lat: 34.68532, lng: 135.83645}},
-      ]
+      center: { lat: 34.7315168, lng: 135.7341339 },
+      // markers: [
+      //   {position: {lat: 34.684, lng: 135.836}},
+      //   {position: {lat: 34.68532, lng: 135.83645}},
+      // ],
+      garbages: [],
     };
   },
+
+  firestore() {
+    return {
+      garbages: db.collection('garbage').orderBy("time", "asc")
+    }
+  }
 };
 </script>
